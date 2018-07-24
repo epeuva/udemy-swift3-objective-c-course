73e448976c1234b5ab17a39973ad454fedec45d1
epeuva <epeuva@gmail.com>
2018-07-24 19:04:47 +0200

    Added BOOL example

--- 141-lesson-16-working-with-varibles/141-lesson-16-working-with-variables/141-lesson-16-working-with-variables/ViewController.m
+++ 141-lesson-16-working-with-varibles/141-lesson-16-working-with-variables/141-lesson-16-working-with-variables/ViewController.m
@@ -24,12 +24,24 @@
     
     self.label1.text = [NSString stringWithFormat:@"%@ %@", string1, string2];
     
-    
     // int
     int int1 = 3;
     int int2 = 10;
     
     self.label2.text = [NSString stringWithFormat:@"%d", int1 + int2];
+    
+    // double
+    double double1 = 100.247;
+    double double2 = 324.546;
+    
+    self.label3.text = [NSString stringWithFormat:@"%.2f %.0f", double1, double2];
+    
+    // boolean
+    BOOL bool1 = NO;
+    BOOL bool2 = false;
+    
+    self.button.enabled = bool2;
+    self.switches.on = bool1;
 }
 
 
