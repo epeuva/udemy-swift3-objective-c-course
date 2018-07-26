a0dd9e79da623de4b70923936cd71a60a095de17
epeuva <epeuva@gmail.com>
2018-07-26 17:25:37 +0200

    Added if with numbers, and, or

--- 143-lesson-16-if-else-or/143-lesson-16-if-else-or/ViewController.m
+++ 143-lesson-16-if-else-or/143-lesson-16-if-else-or/ViewController.m
@@ -19,7 +19,6 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     BOOL state = false;
-    
     if (state == true) {
         self. label1.text = @"Allow access";
     } else {
@@ -27,7 +26,6 @@
     }
     
     NSString *name = @"Epeuva";
-    
     if([name isEqualToString:@"Epeuva"]) {
         self.label2.text = @"Hello Epeuva";
     } else if ([name isEqualToString:@"Steve"]) {
@@ -36,6 +34,30 @@
         self.label2.text = @"Huh, you got a terrible name";
     }
     
+    int number = 999;
+    if(number > 1500) {
+        self.label3.text = @"Unlock level 2";
+    } else {
+        self.label3.text = @"Try again";
+    }
+    
+    NSString *colour = @"Red";
+    if([colour isEqualToString:@"Red"] || [colour isEqualToString:@"Green"]) {
+        self.label4.text = @"Nice color";
+    }
+    
+    NSString *forename = @"Aaron";
+    NSString *surname = @"Caines";
+    
+    if([forename isEqualToString:@"Aaron"] && [surname isEqualToString:@"Caines"]) {
+        self.label5.text = @"Grant access";
+    } else if ([forename isEqualToString:@"Aaron"]) {
+        self.label5.text = @"Surname incorrect!";
+    } else if ([surname isEqualToString:@"Caines"]) {
+        self.label5.text = @"Forename incorrect!";
+    } else {
+        self.label5.text = @"All fields incorrect";
+    }
     
 }
 
