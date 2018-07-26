ac2102d3397bc615f21d2801e241f2dda826731f
epeuva <epeuva@gmail.com>
2018-07-26 11:33:32 +0200

    Added NSArray

--- 142-lesson-16-working-with-arrays/142-lesson-16-working-with-arrays/ViewController.h
+++ 142-lesson-16-working-with-arrays/142-lesson-16-working-with-arrays/ViewController.h
@@ -9,6 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UILabel *label2;
+@property (weak, nonatomic) IBOutlet UILabel *label3;
 
 
 @end
--- 142-lesson-16-working-with-arrays/142-lesson-16-working-with-arrays/ViewController.m
+++ 142-lesson-16-working-with-arrays/142-lesson-16-working-with-arrays/ViewController.m
@@ -17,6 +17,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // NSArray
+    NSArray *array = @[@"Apple", @"Banana", @"Orange"];
+    self.label.text = array[0];
+    
 }
 
 
