afa70fcd056c9464335a2fb377c40c8b8307793c
epeuva <epeuva@gmail.com>
2018-07-26 17:41:15 +0200

    Added two while loops

--- 144-lesson-16-while-loops/144-lesson-16-while-loops/ViewController.m
+++ 144-lesson-16-while-loops/144-lesson-16-while-loops/ViewController.m
@@ -17,6 +17,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    int number = 0;
+    while (number < 5) {
+        NSLog(@"%i", number);
+        number++;
+    }
+    
+    int number1 = 1000;
+    int number2 = 100;
+    while (number1 > 0) {
+        NSLog(@"%i", number1);
+        number1 -= number2;
+    }
+    
 }
 
 
